67b1fb6c4120d516a3d7f5d13af00d3ab697ec81
davimarcilio <davimarcilio.js@gmail.com>
2024-04-13 16:37:00 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -6,53 +6,54 @@ import * as echarts from "echarts";
 const app = express();
 
 app.get("/", async (req, res) => {
-  const chart = echarts.init(null, null, {
-    renderer: "svg",
-    ssr: true,
-    height: 250,
-  });
+  // const chart = echarts.init(null, null, {
+  //   renderer: "svg",
+  //   ssr: true,
+  //   height: 250,
+  // });
 
-  readFile(
-    path.join(
-      __dirname,
-      "./davimarcilio-codetime-records-12_04_2024, 21_25_05.csv"
-    ),
-    "utf8",
-    (error, data) => {
-      if (error) {
-        throw new Error(error.message);
-      }
-      const lines = data.split("\n").slice(1);
+  // readFile(
+  //   path.join(
+  //     __dirname,
+  //     "./davimarcilio-codetime-records-12_04_2024, 21_25_05.csv"
+  //   ),
+  //   "utf8",
+  //   (error, data) => {
+  //     if (error) {
+  //       throw new Error(error.message);
+  //     }
+  //     const lines = data.split("\n").slice(1);
 
-      const fullData = lines.map((line) => {
-        const splittedData = line.split(",");
-        return {
-          editor: splittedData.at(0),
-          platform: splittedData.at(1),
-          project: splittedData.at(2),
-          relative_file: splittedData.at(3),
-          language: splittedData.at(4),
-          event_time:
-            splittedData.at(5) &&
-            `${dayjs(Number(splittedData.at(5))).format(
-              "YYYY-MM-DD"
-            )}T00:00:00Z`,
-        };
-      });
-      const allDays = Array.from(
-        new Set(fullData.filter((e) => !!e.event_time).map((e) => e.event_time))
-      ) as String[];
-      const daysWithTimeInDay = allDays.map((day) => ({
-        time: day,
-        by: "",
-        duration: fullData.filter((e) => e.event_time === day).length * 60000,
-      }));
+  //     const fullData = lines.map((line) => {
+  //       const splittedData = line.split(",");
+  //       return {
+  //         editor: splittedData.at(0),
+  //         platform: splittedData.at(1),
+  //         project: splittedData.at(2),
+  //         relative_file: splittedData.at(3),
+  //         language: splittedData.at(4),
+  //         event_time:
+  //           splittedData.at(5) &&
+  //           `${dayjs(Number(splittedData.at(5))).format(
+  //             "YYYY-MM-DD"
+  //           )}T00:00:00Z`,
+  //       };
+  //     });
+  //     const allDays = Array.from(
+  //       new Set(fullData.filter((e) => !!e.event_time).map((e) => e.event_time))
+  //     ) as String[];
+  //     const daysWithTimeInDay = allDays.map((day) => ({
+  //       time: day,
+  //       by: "",
+  //       duration: fullData.filter((e) => e.event_time === day).length * 60000,
+  //     }));
 
-      chart.setOption(getCalendarOptions(daysWithTimeInDay, 1080));
-      const svgChart = chart.renderToSVGString();
-      res.send(svgChart);
-    }
-  );
+  //     chart.setOption(getCalendarOptions(daysWithTimeInDay, 1080));
+  //     const svgChart = chart.renderToSVGString();
+  //     res.send(svgChart);
+  //   }
+  // );
+  return res.send("test");
 });
 
 app.listen(3000);

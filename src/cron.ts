5498a8b389b6787708f041348f76296c19b53dcc
davimarcilio <davimarcilio.js@gmail.com>
2024-04-14 12:16:17 -0300

    Vercel cron fixes

---
+++ src/cron.ts
@@ -0,0 +1,46 @@
+import { api } from "./lib/api";
+import { prisma } from "./lib/prisma";
+
+async function updateDatabase() {
+  const { data: csv, request } = await api.post<string>(
+    "/user/records/export",
+    null,
+    {
+      headers: {
+        Cookie: `CODETIME_SESSION=${process.env.CODETIME_SESSION}`,
+      },
+    }
+  );
+
+  const lines = csv.split("\n").slice(1);
+
+  const fullData = lines
+    .filter((line) => line.split(",").length === 6)
+    .map((line) => {
+      const splittedData = line.split(",");
+      return {
+        editor: splittedData.at(0),
+        platform: splittedData.at(1),
+        project: splittedData.at(2),
+        relative_file: splittedData.at(3),
+        language: splittedData.at(4),
+        event_time: Number(splittedData.at(5) ?? 0),
+      };
+    });
+
+  const lastRecord = await prisma.records.findFirst({
+    orderBy: {
+      event_time: "desc",
+    },
+  });
+
+  const filteredData = fullData.filter(
+    (e) => e.event_time > (lastRecord?.event_time ?? 0)
+  );
+
+  await prisma.records.createMany({
+    data: filteredData,
+  });
+}
+
+updateDatabase();
--- src/index.ts
+++ src/index.ts
@@ -49,48 +49,6 @@ app.get("/graph", middleware("24 hours"), async (req, res) => {
 
 app.listen(3000, async () => {
   console.log("Api listen on 3000, starting cronjobs");
-
-  schedule("0 0 * * *", async () => {
-    const { data: csv, request } = await api.post<string>(
-      "/user/records/export",
-      null,
-      {
-        headers: {
-          Cookie: `CODETIME_SESSION=${process.env.CODETIME_SESSION}`,
-        },
-      }
-    );
-
-    const lines = csv.split("\n").slice(1);
-
-    const fullData = lines
-      .filter((line) => line.split(",").length === 6)
-      .map((line) => {
-        const splittedData = line.split(",");
-        return {
-          editor: splittedData.at(0),
-          platform: splittedData.at(1),
-          project: splittedData.at(2),
-          relative_file: splittedData.at(3),
-          language: splittedData.at(4),
-          event_time: Number(splittedData.at(5) ?? 0),
-        };
-      });
-
-    const lastRecord = await prisma.records.findFirst({
-      orderBy: {
-        event_time: "desc",
-      },
-    });
-
-    const filteredData = fullData.filter(
-      (e) => e.event_time > (lastRecord?.event_time ?? 0)
-    );
-
-    await prisma.records.createMany({
-      data: filteredData,
-    });
-  });
 });
 
 // async function readDataFile() {
